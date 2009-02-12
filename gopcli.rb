797a7913c4f954fc4fb3fdb1d103c16ceec14db8
Ari Turetzky <abturet@schlemil.(none)>
2009-02-12 14:55:00 -0600

    adding cli

---
+++ gopcli.rb
@@ -0,0 +1,8 @@
+require 'gopapi'
+
+api_token = "F9F63463-0096-450C-9DE9-E1A35B808046"
+#puts Gopapi.member_getall('F9F63463-0096-450C-9DE9-E1A35B808046')
+puts '*******************   BREAK ************************'
+
+##puts Gopapi.member_get(api_token,'00','AK')
+puts Gopapi.bill_getall(api_token,'111')
