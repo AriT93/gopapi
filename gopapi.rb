e94f55c16ee846f718b21ebc30bd1bf94723207a
Arit93 <Arit93@yahoo.com>
2009-02-17 07:28:14 -0600

    updates add check for error and raise

--- gopapi.rb
+++ gopapi.rb
@@ -1,23 +1,22 @@
 module Gopapi
-#module to wrap the calls to gop api should be a class
-#still reading best way to implement that
+  #module to wrap the calls to gop api should be a class
+  #still reading best way to implement that
   require 'net/http'
   require 'uri'
 
   def self.getGOPXML(method_name,data)
     uri = URI.parse(ENV['http_proxy'])
-    @proxy_host = uri.host
-    @proxy_port = uri.port
-    @proxy_user,@proxy_pass = uri.userinfo.split(/:/) if uri.userinfo
+    proxy_host = uri.host
+    proxy_port = uri.port
+    proxy_user,proxy_pass = uri.userinfo.split(/:/) if uri.userinfo
 
-    Net::HTTP::Proxy(@proxy_host,@proxy_port,@proxy_user,@proxy_pass).start('www.gop.gov'){ |w|
-      puts method_name
+    Net::HTTP::Proxy(proxy_host,proxy_port,proxy_user,proxy_pass).start('www.gop.gov'){ |w|
       res = w.post(method_name,data)
-      puts res.body
+      raise RuntimeError.new(res.body) if res.body.match(/^ERROR:*/)
+      res.body
     }
   end
-  def self.getProxyInfo
-  end
+
   def self.member_getall(api_token)
     getGOPXML('/api/member.getall','api_token='+api_token)
   end
@@ -31,9 +30,30 @@ module Gopapi
     getGOPXML('/api/member.group','api_token=' + api_token + '&member_state=' + member_state + '&member_rssurl=' + member_rssurl + '&member_gender=' + member_gender + '&member_status=' + member_status)
   end
   def self.bill_get(api_token,bill_number,bill_congress)
-    getGOPXML('/api/bill.get','api_token='+ api_token + '&bill_number=' + bill_number + '&bill_congress=' + bill_congress)
+     getGOPXML('/api/bill.get','api_token='+ api_token + '&bill_number=' + bill_number + '&bill_congress=' + bill_congress)
   end
   def self.bill_getall(api_token,bill_congress)
-   getGOPXML('/api/bill.getall','api_token='+ api_token + '&bill_congress=' + bill_congress)
+    getGOPXML('/api/bill.getall','api_token='+ api_token + '&bill_congress=' + bill_congress)
+  end
+  def self.bill_keyword(api_token,bill_keyword)
+    getGOPXML('/api/bill.keyword','api_token=' + api_token + '&bill_keyword=' + bill_keyword)
+  end
+  def self.vote_getall(api_token,vote_congress)
+    getGOPXML('/api/vote.getall','api_token=' + api_token + '&vote_congress=' + vote_congress)
+  end
+  def self.vote_member(api_token,vote_roll,vote_congress,member_state,member_district)
+    getGOPXML('/api/vote.member','api_token=' + api_token + '&vote_roll=' + vote_roll + '&vote_congress=' + vote_congress + '&member_state=' + member_state + '&member_district=' + member_district)
+  end
+  def self.legdigest_get(api_token,digest_year,digest_month,digest_day)
+    getGOPXML('/api/legdigest.get','api_token=' + api_token + '&digest_year=' + digest_year + '&digest_month=' + digest_month + '&digest_day=' + digest_day)
+  end
+  def self.legdigest_getall(api_token)
+    getGOPXML('/api/legdigest.getall', 'api_token=' + api_token)
+  end
+  def self.committee_getall(api_token)
+    getGOPXML('/api/committee.getall','api_token=' + api_token)
+  end
+  def self.committee_members(api_token,committee_name,committee_type,committee_parent)
+    getGOPXML('/api/committee.members', 'api_token=' + api_token + '&committee_name=' + committee_name + '&committee_type=' + committee_type + '&committee_parent=' + committee_parent)
   end
 end
--- gopcli.rb
+++ gopcli.rb
@@ -1,8 +1,9 @@
 require 'gopapi'
 
 api_token = "F9F63463-0096-450C-9DE9-E1A35B808046"
-puts Gopapi.member_getall('F9F63463-0096-450C-9DE9-E1A35B808046')
-puts '*******************   BREAK ************************'
+#puts Gopapi.member_getall('F9F63463-0096-450C-9DE9-E1A35B808046')
+#puts '*******************   BREAK ************************'
 
-puts Gopapi.member_get(api_token,'00','AK')
-puts Gopapi.bill_getall(api_token,'111')
+##puts Gopapi.member_get(api_token,'00','AK')
+#puts Gopapi.bill_get(api_token,"HR1",'111')
+puts Gopapi.vote_getall(api_token,'111')
